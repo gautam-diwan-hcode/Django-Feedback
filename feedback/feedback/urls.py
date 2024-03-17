f706b7a70ffc6025a4c8e40f4675e66ab44aba0d
Gautam Diwan <gautam.diwan@hcode.tech>
2024-03-17 16:18:34 +0530

    Add sessions

--- feedback/feedback/urls.py
+++ feedback/feedback/urls.py
@@ -21,6 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reviews/', include('reviews.urls')),
+    path('', include('reviews.urls')),
     path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
